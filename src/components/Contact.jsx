a277a8786d0ec88b17f52356ba62f06b9fcdbf66
aarrrryyaaaaa <aryatonys01@gmail.com>
2025-12-16 10:22:39 +0800

    Update Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -206,7 +206,7 @@ export default function Contact() {
 
                         {/* Instagram */}
                         <SocialIcon
-                            href="https://instagram.com/aryats"
+                            href="https://www.instagram.com/aarrrryyaaaaa_?igsh=N21wZ214MjB6NWZy&utm_source=qr"
                             colorClass="hover:border-pink-500/50 hover:bg-pink-900/20 hover:text-pink-400"
                             path={<><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></>}
                         />
